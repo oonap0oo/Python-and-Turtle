3d6b7c5d7629af1981041b544f666a9242f5cd34
oonap0oo <vnap0v@gmail.com>
2025-07-13 14:47:54 +0200

    Add files via upload

--- truchet_tiles2_random_with_save.py
+++ truchet_tiles2_random_with_save.py
@@ -1,4 +1,6 @@
 # Randomly generated Truchet Tiles using Python & Turtle
+# when key <s> is pressed save the pattern to a text file
+# mouse click in the window to stop the script
 import turtle as tl
 import random
 
@@ -6,11 +8,14 @@ import random
 tile_size = 50 # size of one side of square tile measured in pixels
 window_width_tiles = 36 # window width measured in number of tiles
 window_height_tiles = 18
+pattern_half_number_of_rows = 3
+pattern_half_number_of_columns = 3
 color_bg = "#fffff8"
 color_fg = "black"
 line_width = 2
 title = "Randomly generated Truchet Tiles using Python & Turtle"
 time_delay_ms = 1500
+patterns_output_file_name = "patterns.txt"
 
 # set up turtle
 def init():
@@ -22,6 +27,8 @@ def init():
     screen.setup(width = window_width_tiles * tile_size , height = window_height_tiles * tile_size)
     # assign mouse click handler for terminating script
     screen.onclick(mouse_click_handler)
+    # assign key <s> press event handler for saving pattern to file
+    tl.onkey(key_save_pattern_handler, "s")
     # set speed
     tl.speed(0) # fastest drawing speed
     tl.tracer(0) # pauze screen updates untill tl.update() command is encountered, speeds up drawing
@@ -115,7 +122,7 @@ def generate_random_pattern(half_number_of_rows, half_number_of_columns):
         new_row = []
         for column in range(half_number_of_columns):
             new_row.append(value)
-            value += random.choice([-3,-1,1,3]) # next tile based on previous either add or subtract 3
+            value += random.choice([-3,-1,1,3]) # next tile based on previous either add or subtract 1 or 3
             value = (value - 1) % 4 + 1 # tile value wraps around if outside 1..4
         for column in range(half_number_of_columns - 1, -1, -1):
             first_tile = new_row[column]
@@ -131,20 +138,33 @@ def generate_random_pattern(half_number_of_rows, half_number_of_columns):
 
 # mouse click in the window to stop the script
 def mouse_click_handler(x,y):
-    quit()
+    update()
+
+# when key <s> is pressed save the pattern to a text file
+def key_save_pattern_handler():
+    global pattern
+    print(*pattern, sep="\n") # print generated pattern to console
+    print()
+    with open(patterns_output_file_name, mode = "a") as file:
+        file.write(str(pattern))
+        file.write("\n")
+    print("Pattern saved to", patterns_output_file_name),"\n\n"
 
 # loop through random patterns
 def update():
-    pattern = generate_random_pattern(3,3)
-    print(*pattern, sep="\n") # print generated pattern to console
-    print()
+    global pattern
+    pattern = generate_random_pattern(pattern_half_number_of_rows, pattern_half_number_of_columns)
     rows = window_height_tiles // len(pattern) + 1
     cols = window_width_tiles // len(pattern[0]) + 1
     repeat_pattern(pattern, rows, cols)
-    tl.ontimer(update, time_delay_ms) # set timer for next update
+    tl.listen()
     
    
 init()
+
+print("""Randomly generated Truchet Tiles using Python & Turtle
+when key <s> is pressed save the pattern to a text file
+mouse click in the window to stop the script""")
 
 update() # first time call update immediatly
 
